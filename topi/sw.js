bf1eeb566e2ab8be2dcd7100feb4d32cabccd4b4
stevoblevo <stevoblevo@gmail.com>
2026-09-20 22:11:25 -0600

    Complete Topi Berry PWA/container source

---
+++ topi/sw.js
@@ -0,0 +1,1 @@
+const C='topi-gh-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
